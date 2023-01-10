741f6820c0d4e5d8247eba19effcac7d15564a6e
lgoenaga <luis.goenaga@est.iudigital.edu.co>
2023-01-10 17:05:25 -0500

    incorporación de mobile-menu y evento click sobre menu-icon

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuIcon = document.querySelector('.menu');
 
 navbarEmail.addEventListener( 'click', toggleDesktopMenu );
+menuIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu () {
-    desktopMenu.classList.toggle('inactive');
+    desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle( 'inactive' );
 }
